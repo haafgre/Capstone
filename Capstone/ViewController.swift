543267dc43a4e8a4f26dea4300e32dedf88126f3
haafgre <haafg20@gmail.com>
2022-05-01 22:43:28 -0500

    Added Logo and images

--- Capstone/ViewController.swift
+++ Capstone/ViewController.swift
@@ -9,8 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var loadIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //loadIcon.contentMode = .scaleAspectFit
+        //loadIcon.image = UIImage(named: "Icon1x")
         // Do any additional setup after loading the view.
         //let fileUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     }
